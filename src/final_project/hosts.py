7dd9eb202457d1b667ff9b3fb6ae16dd34c1f940
albarodriguez09-art <alba.rodriguez09@estudiant.upf.edu>
2025-11-24 19:33:18 +0100

    modificación documento hosts

--- src/final_project/hosts.py
+++ src/final_project/hosts.py
@@ -43,8 +43,8 @@ class Host:
             # [Inicio] Lógica para el Graph 2 V1: Modificación de la Regla
             # Descomenta las siguientes 2 líneas y comenta la anterior para probar V1
             # ----------------------------------------------------------------------
-            # if city.is_v1_active and place.area != self.area_of_origin:
-            #     continue # El host solo puede comprar en su área de origen (V1)
+            if city.is_v1_active and place.area != self.area_of_origin:
+                continue # El host solo puede comprar en su área de origen (V1)
             # ----------------------------------------------------------------------
             # [Fin] Lógica V1
             # ----------------------------------------------------------------------
